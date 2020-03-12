bbb0e5b7e01b342067e2e910e0c5975f699bee3b
Javiig13 <javieriglesiaslopez13@gmail.com>
2020-03-12 20:43:25 +0100

    Pedidos terminado.

---
+++ Tienda/Tienda/Pages/Orders/Details.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tienda.DataAccess;
+using Tienda.Models;
+
+namespace Tienda
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DatabaseContext _context;
+
+        public DetailsModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ProductDetail> Products { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Products = await _context.ProductOrder.Where(m => m.OrderId == id).Select(c => new ProductDetail()
+            {
+                Amount = c.Amount,
+                Image = c.Product.Image,
+                Name = c.Product.Name,
+                Price = c.Product.Price,
+                TotalPrice = c.Amount * c.Product.Price
+
+            }).ToListAsync();
+
+            if (Products == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+
+    public class ProductDetail : Product
+    {
+        public int Amount { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Tienda/Tienda/Pages/Orders/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tienda.DataAccess;
+
+namespace Tienda
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DatabaseContext _context;
+
+        public IndexModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<OrderDetail> OrderDetails { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            List<OrderDetail> details = new List<OrderDetail>();
+
+            var userId = int.Parse(HttpContext.Session.GetString("UserSession"));
+
+            var orders = await _context.ProductOrder
+                .Include(p => p.Order)
+                .Include(p => p.Product)
+                .Where(x => x.Order.Customer.Id == userId)
+                .ToListAsync();
+
+            var result = orders.GroupBy(x => x.Order.Id).Select(g => (
+                OrderId: g.Key,
+                OrderDate: g.Select(c => c.Order.OrderDate).FirstOrDefault(),
+                OrderPrice: g.Sum(x => x.Amount > 0 ? x.Amount : x.Amount)
+            )).ToList();
+
+            result.ForEach(r =>
+            {
+                details.Add(new OrderDetail()
+                {
+                    OrderDate = r.OrderDate.Day + "/" + r.OrderDate.Month + "/" + r.OrderDate.Year,
+                    OrderId = r.OrderId,
+                    OrderPrice = r.OrderPrice
+                });
+            });
+
+            OrderDetails = details;
+        }
+    }
+
+    public class OrderDetail
+    {
+        public int OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public decimal OrderPrice { get; set; }
+    }
+}
